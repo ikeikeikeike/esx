4a8c39554d6d43074b4ad874a208358b3a7208d2
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-11-16 05:05:43 +0900

    response

--- lib/esx/model/response.ex
+++ lib/esx/model/response.ex
@@ -1,16 +1,15 @@
 defmodule ESx.Model.Response do
 
-  defstruct [:took, :timed_out, :shards, :hits, :total, :max_score, :response]
+  defstruct [:took, :timed_out, :shards, :hits, :total, :max_score]
 
   def parse({:ok, %{} = rsp}) do
     %__MODULE__{
-      took: rsp["took"],
-      timed_out: rsp["timed_out"],
-      shards: rsp["shards"],
       hits: rsp["hits"]["hits"],
       total: rsp["hits"]["total"],
       max_score: rsp["hits"]["max_score"],
-      response: rsp,
+      took: rsp["took"],
+      timed_out: rsp["timed_out"],
+      shards: rsp["_shards"],
     }
   end
 
