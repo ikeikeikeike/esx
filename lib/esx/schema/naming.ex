d898d44e6a9e1452b2bd6c82ab34b5f99c974515
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-11-21 22:37:30 +0900

    bugfix

--- lib/esx/schema/naming.ex
+++ lib/esx/schema/naming.ex
@@ -9,22 +9,27 @@ defmodule ESx.Schema.Naming do
 
       Module.register_attribute(__MODULE__, :es_document_type, accumulate: false)
       Module.register_attribute(__MODULE__, :es_index_name, accumulate: false)
+
+      @before_compile unquote(__MODULE__)
     end
   end
 
   defmacro index_name(name) do
     quote do
       Module.put_attribute(__MODULE__, :es_index_name, "#{unquote(name)}")
-
-      def __es_naming__(:index_name) do
-        @es_index_name || Funcs.to_index_name(__MODULE__)
-      end
     end
   end
   defmacro document_type(name) do
     quote do
       Module.put_attribute(__MODULE__, :es_document_type, "#{unquote(name)}")
+    end
+  end
 
+  defmacro __before_compile__(_env) do
+    quote do
+      def __es_naming__(:index_name) do
+        @es_index_name || Funcs.to_index_name(__MODULE__)
+      end
       def __es_naming__(:document_type) do
         @es_document_type || Funcs.to_document_type(__MODULE__)
       end
