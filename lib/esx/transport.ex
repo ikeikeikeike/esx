bea8d477feeeb13c1e594c94ed652de1b22cbe00
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-12-12 16:10:48 +0900

    wip performe request

--- lib/esx/transport.ex
+++ lib/esx/transport.ex
@@ -1,14 +1,22 @@
 defmodule ESx.Transport do
+  @moduledoc "ESx.Transport"
+
   use ESx.Transport.Config
 
   defmodule State do
+    @moduledoc "State"
+
     use ESx.Transport.Statex, [
       :last_request_at, :resurrect_after,
       :counter, :reload_after, :reload,
-      :randomize_hosts
+      :randomize_hosts,
+      :max_retries,
+      :retry_on_status,
     ]
     def initialize_state(args) do
       Keyword.merge args, [
+        max_retries: 5,
+        retry_on_status: [],
         randomize_hosts: false,
         last_request_at: :os.system_time(:seconds),
         resurrect_after: 60,
@@ -19,13 +27,11 @@ defmodule ESx.Transport do
     end
   end
 
-  alias ESx.Transport.State
-  alias ESx.Transport.Sniffer
-  alias ESx.Transport.Connection
-
   import ESx.Checks, only: [present?: 1]
 
   require Logger
+
+  alias ESx.Transport.{State, Sniffer, Connection}
 
   defstruct [
     method: "GET",
@@ -41,7 +47,7 @@ defmodule ESx.Transport do
     struct __MODULE__, Keyword.merge(args, conn: conn)
   end
 
-  defp conn do
+  def conn do
     s = State.state
 
     if :os.system_time(:seconds) > s.last_request_at + s.resurrect_after do
@@ -84,18 +90,8 @@ defmodule ESx.Transport do
     |> Enum.filter_map(& &1.dead, &Connection.resurrect!/1)
   end
 
-  # TODO: Count dead connection
-  #
   def perform_request(%__MODULE__{} = ts, method, path, params \\ %{}, body \\ nil) do
     method = if "GET" == method && body, do: ts.method, else: method
-
-    headers = [{"Content-Type", "application/json"}, {"Connection", "keep-alive"}]
-    options = [hackney: [pool: ts.conn.pidname]]
-
-    uri =
-      URI.merge(ts.conn.url, path)
-      |> URI.merge("?" <> URI.encode_query params)
-      |> URI.to_string
     body =
       case body do
         body when is_map(body) ->
@@ -104,21 +100,70 @@ defmodule ESx.Transport do
           body || ""
       end
 
-    if ts.trace, do: traceout method, uri, body
-
-    case method do
-      "GET"    -> ts.conn.client.request :get,    uri, body, headers, options
-      "PUT"    -> ts.conn.client.request :put,    uri, body, headers, options
-      "POST"   -> ts.conn.client.request :post,   uri, body, headers, options
-      "HEAD"   -> ts.conn.client.request :head,   uri, body, headers, options
-      "DELETE" -> ts.conn.client.request :delete, uri, body, headers, options
-      method   -> {:error, %ArgumentError{message: "Method #{method} not supported"}}
-    end
+    do_perform_request method, path, params, body
   end
   def perform_request!(%__MODULE__{} = ts, method, path, params \\ %{}, body \\ nil) do
     case perform_request(ts, method, path, params, body) do
       {:ok, rsp} -> rsp
       {:error, err} -> raise err
+    end
+  end
+
+  defmodule ServerError do
+    defexception message: "no message"
+  end
+
+  defp do_perform_request(method, path, params, body, tries \\ 0) do
+    tries = tries + 1
+
+    c = conn()  # TODO: needs error management
+
+    headers = [{"Content-Type", "application/json"}, {"Connection", "keep-alive"}]
+    options = [hackney: [pool: c.pidname]]
+    uri =
+      c.url
+      |> URI.merge(path)
+      |> URI.merge("?" <> URI.encode_query params)
+      |> URI.to_string
+
+    resp =
+      case method do
+        "GET"    -> c.client.request :get,    uri, body, headers, options
+        "PUT"    -> c.client.request :put,    uri, body, headers, options
+        "POST"   -> c.client.request :post,   uri, body, headers, options
+        "HEAD"   -> c.client.request :head,   uri, body, headers, options
+        "DELETE" -> c.client.request :delete, uri, body, headers, options
+        method   -> {:error, %ArgumentError{message: "Method #{method} not supported"}}
+      end
+
+    s = State.state
+
+    case resp do
+      {:ok, %HTTPoison.Response{status_code: code} = resp} when code < 300 ->
+        resp
+
+        # TODO:
+        # if ts.trace, do: traceout method, uri, body
+
+        # connection.healthy! if connection.failures > 0
+
+      # retry_on_status
+      {:ok, %HTTPoison.Response{status_code: code}} when code >= 300 ->
+        if tries <= s.max_retry and code in s.retry_on_status do
+          Logger.debug "retry in #{code} status code."
+          do_perform_request method, path, params, body, tries
+        else
+          {:error, %ServerError{message: "Method #{method} not supported"}}
+        end
+
+      # failure
+      {:error, %HTTPoison.Error{reason: reason} = perr} ->
+        if tries <= s.max_retry do
+          Logger.debug reason
+          do_perform_request method, path, params, body, tries
+        else
+          {:error, perr}
+        end
     end
   end
 
