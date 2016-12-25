f5180ea097e1375e2367b0dd87ce06ac56fe5a31
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-12-25 23:26:48 +0900

    update

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ESx.Mixfile do
 
   def project do
     [app: :esx,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
